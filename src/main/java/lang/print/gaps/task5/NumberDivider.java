58a591462409e07702e352ebe863fd9cd2292eae
guilt <gulilty4all@gmail.com>
2022-08-17 11:44:40 +0600

    solution fifth try

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        float result = (float)toBeDivided / 5;
+        double result = toBeDivided / 5.0;
 
         System.out.println(result);
     }
