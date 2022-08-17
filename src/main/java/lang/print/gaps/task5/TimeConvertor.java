0c5925937668efb33b1c4fc85b9d3bba3b9e1d6c
guilt <gulilty4all@gmail.com>
2022-08-17 10:38:34 +0600

    solution third try

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        double result = minutes * 60;
-        System.out.println(result);
+        float seconds = minutes * 60;
+        System.out.println(seconds);
     }
 }
